a3c481b74bfabb4fcbcf80b1bfc5bbc3a748bb50
nekonee <haniasa96@gmail.com>
2018-01-24 21:27:04 +0100

    Quotes to choose from

---
+++ quotes.py
@@ -0,0 +1,29 @@
+first=[
+    "To, o czym media nie chca powiedziec, to to, ze ",
+    "Jak mawia kolega Michalkiewicz, to przez razwiedke ",
+    "Jakby powiedzial jego ekscelencja doktor Jozef Goebbels ",
+    "Zgodnie z badaniami Instytutu Socjocybernetyki ",
+    "Wbrew badaniom eurokratow ktorzy robia gigantyczny przekret na GLOBCIO ",
+    "Choc Banda Czworga stara sie o tym nei mowic, to ",
+    "Grodzkie ani- tfu!- geje nie powiedza nam, ze ",
+]
+
+second=[
+    "panstwo w podatkach kradnie nam 90% dochodow ",
+    "na nasz koszt urzadza sie zawody niszczace wizerunek bialego, silnego czlowieka ",
+    "dzieci zdrowe zarazaja sie niepelnosprawnoscia od inwalidow ",
+    "lekka pedofilia nie jest szkodliwa spolecznie ",
+    "generala Kiszczaka nalezy podziwiac ",
+    "wszystkie think-tanki sa skazone socjalizmem ",
+    "na dnie oceanu, w Rowie Marianskim, zyja sobie ru-ru-rurkowce ",
+]
+
+third=[
+    "czego nie powstydzilby sie s.p. Adolf Hitler.",
+    "o czym warto pamietac w kontekscie zmanipulowanych sondazy.",
+    "podczas gdy za WC za Stalina mozna bylo swobodnie produkowac oscypki.",
+    "czego probke mielismy na placu Tienanmen.",
+    "-ale nie bede przeciez zastepowac panskich nauczycieli!",
+    "choc do Murzynki nie moglem sie przemoc.",
+    "a kobieta nasiaka pogladami mezczyzny przez nasienie.",
+]
